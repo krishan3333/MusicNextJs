f054208ae880be2bdc3b275a07d783f6214e2e89
krishan3333 <work.krishansingh@gmail.com>
2025-10-10 01:48:14 +0530

    instructor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import HeroSection from './components/HeroSection';
 import WhyChooseUs from './components/WhyChooseUS';
 import TestimonialCard from './components/TestimonialCard';
 import UpcomingWebinars from './components/UpcomingWebinars';
+import Instructors from './components/Instructors';
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -13,6 +15,7 @@ export default function Home() {
       <WhyChooseUs/>
       <TestimonialCard/>
       <UpcomingWebinars />
+      <Instructors/>
     </main>
   );
 }
